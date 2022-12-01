545a832ceefac978998f1d575cf78a7ddd618b07
draqaah <108563896+draqaah@users.noreply.github.com>
2022-11-30 22:15:06 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+	<title>HACKED</title>
+	<style>
+	    iframe {
+		    display:none;
+	    }
+		.glow {
+  font-size: 40px;
+  color: #fff;
+  text-align: center;
+  -webkit-animation: glow 1s ease-in-out infinite alternate;
+  -moz-animation: glow 1s ease-in-out infinite alternate;
+  animation: glow 1s ease-in-out infinite alternate;
+}
+
+@-webkit-keyframes glow {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+  }
+  to {
+    text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
+  }
+}
+	</style>
+</head>
+
+<body background="https://i.imgur.com/5rZ91h5.gif">
+
+</script>
+	<iframe width="200" height="113" src="https://www.youtube.com/embed/hZsaPu-kthY?playlist=hZsaPu-kthY&amp;autoplay=1&amp;loop=1&amp;rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+	<center>
+		<b><p class="glow" style=color:re>✞✞SUPRISED? YEE, LUCIFER WAZ HERE✞✞</p></b>
+		<h2 style=color:green;>HACKED BY T.ME/LEAKINFORMATION MEMBERS</h2>
+		<img src="https://i.imgur.com/OhoiDOT.png">
+		<h3 style=color:grey; >*****地獄 LEAKINFORMATION OPERATIONS******</h3>
+	</center>
+	<center>
+	<audio id="myAudio" controls autoplay>
+ 	 <source src="rap.mp3" type="audio/mpeg">
+	</audio>
+	<script>
+	function myFunction() {
+  	var x = document.getElementById("myAudio").autoplay;
+	}	
+	
+	</center>
+	
+</body>
+</html>
